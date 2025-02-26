df8c7d881f1656f1d8d7589ae203087c4514ad45
Purushottam <pallipurushotham333@gmail.com>
2025-02-26 09:54:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 import express from "express";
-import mongoose from "mongoose";
-
-
+// import mongoose from "mongoose";
 const app = express()
+
+
+const users = [
+    {username: "alice", age: 25, email: "alice@example.com"},
+    {username: "bob", age: 30, email: "alice@example.com"},
+    {username: "charlie", age:28, email: "charlie@example.com"}
+]
+
+app.get("/user" , (req,res) => {
+    res.send("server is running")
+})
+
+
+app.get("/user" , (req,res) => {
+    if(!req.query.user) {
+        return res.status(404).json({message: "user parameter cannot be empty"})
+    }
+
+    const user = users.find(u => {u.username === req.query.user})
+
+    if(user){
+        res.json({message: "user found", data: user})
+    } else{
+        res.status(404).json(message: "user not found" )
+    }
+
+
+})
+
+
+
+
 
 app.use(express.json())
 
